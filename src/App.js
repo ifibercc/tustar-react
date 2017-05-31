dbb019ceca036a1d45d8d02c0fc860644d315aba
jingchu.fhb <jingchu.fhb@alibaba-inc.com>
2017-05-31 15:37:06 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
+import React, {Component} from 'react';
+import {Layout, Menu, Icon} from 'antd';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import RouterContainer from './routes';
 import './App.css';
+
+const {Header, Sider, Content, Footer} = Layout;
+// 通过接口获取
+const navList = [
+  {
+    id: '001',
+    url: '/public',
+    icon: 'user',
+    text: 'public'
+  }, {
+    id: '002',
+    url: '/login',
+    icon: 'home',
+    text: 'login'
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <Router>
+        <Layout style={{
+          height: '100vh'
+        }}>
+          <Sider style={{
+            overflow: 'auto'
+          }}>
+            <div className="logo"></div>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              {navList.map(nav => (
+                <Menu.Item key={nav.id}>
+                  <Icon type={nav.icon}/>
+                  <span className="nav-text">
+                    <Link to={nav.url}>{nav.text}</Link>
+                  </span>
+                </Menu.Item>
+              ))}
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{
+              background: '#FFF',
+              padding: 0
+            }}/>
+            <Content style={{
+              margin: '24px 16px 0',
+              overflow: 'initial'
+            }}>
+              <div style={{
+                padding: 24,
+                background: '#FFF',
+                textAlign: 'center'
+              }}>
+                <RouterContainer/>
+              </div>
+            </Content>
+            <Footer style={{
+              textAlign: 'center'
+            }}>
+              Ant Design ©2016 Created by Ant UED
+            </Footer>
+          </Layout>
+        </Layout>
+      </Router>
     );
   }
 }
